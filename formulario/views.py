2bb33677abd7ce8fbf378db2435f88cd84969f68
duridev <duridev@outlook.com>
2026-02-23 23:44:08 -0300

    Add index view to render ContactoForm in index.html template

--- formulario/views.py
+++ formulario/views.py
@@ -2,15 +2,16 @@ from django.shortcuts import render
 from .forms import ContactoForm
 
 def index(request):
-    return render(request, 'formulario/index.html')
+    form = ContactoForm()
+    return render(request, 'formulario/index.html', {'form' : form})
 
-def contacto_view(request):
-    if request.method == "POST":
-        form = ContactoForm(request.POST)
-        if form.is_valid():
-            nombre = form.cleaned_data["nombre"]
-    else:
-        form = ContactoForm()
+# def contacto_view(request):
+#     if request.method == "POST":
+#         form = ContactoForm(request.POST)
+#         if form.is_valid():
+#             nombre = form.cleaned_data["nombre"]
+#     else:
+#         form = ContactoForm()
         
-    return render(request, "formulario.html", {"form": form})
+#     return render(request, "formulario.html", {"form": form})
 
