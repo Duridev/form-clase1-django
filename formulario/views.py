179b443eb75182695c7d67b434e1c8a000ed9efb
duridev <duridev@outlook.com>
2026-02-23 22:29:46 -0300

    Fix ContactoForm field definition and update contacto_view logic

--- formulario/views.py
+++ formulario/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from .forms import ContactoForm
 
 def contacto_view(request):
-    form = ContactoForm()
+    if request.method == "POST":
+        form = ContactoForm(request.POST)
+        if form.is_valid():
+            nombre = form.cleaned_data["nombre"]
+    else:
+        form = ContactoForm()
+        
     return render(request, "formulario.html", {"form": form})
 
