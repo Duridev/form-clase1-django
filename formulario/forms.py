92572fafd049c9812460da070f6fed25da211506
duridev <duridev@outlook.com>
2026-02-25 00:10:13 -0300

    Add exito view and template; update ContactoForm to include label for nombre

--- formulario/forms.py
+++ formulario/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class ContactoForm(forms.Form):
-    nombre = forms.CharField(max_length=100)
+    nombre = forms.CharField(label='nombre', max_length=100)
     email = forms.EmailField()
     mensaje = forms.CharField(widget=forms.Textarea)
--- formulario/views.py
+++ formulario/views.py
@@ -7,7 +7,10 @@ def index(request):
         form = ContactoForm(request.POST)
         if form.is_valid():
             nombre = form.cleaned_data["nombre"]
+            return render(request, "formulario/exito.html")
     else:
         form = ContactoForm()
     return render(request, "formulario/index.html", {"form": form})
 
+def exito(request):
+    return render(request, "formulario/exito.html")
