1de930aba848d5014fd7e20c2f07276b57c9af47
duridev <duridev@outlook.com>
2026-02-24 23:29:47 -0300

    Refactor index view to handle form submission and validation in views.py

--- formulario/views.py
+++ formulario/views.py
@@ -1,17 +1,13 @@
 from django.shortcuts import render
 from .forms import ContactoForm
 
-def index(request):
-    form = ContactoForm()
-    return render(request, 'formulario/index.html', {'form' : form})
 
-# def contacto_view(request):
-#     if request.method == "POST":
-#         form = ContactoForm(request.POST)
-#         if form.is_valid():
-#             nombre = form.cleaned_data["nombre"]
-#     else:
-#         form = ContactoForm()
-        
-#     return render(request, "formulario.html", {"form": form})
+def index(request):
+    if request.method == "POST":
+        form = ContactoForm(request.POST)
+        if form.is_valid():
+            nombre = form.cleaned_data["nombre"]
+    else:
+        form = ContactoForm()
+    return render(request, "formulario/index.html", {"form": form})
 
