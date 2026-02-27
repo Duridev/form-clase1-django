d4ac2626491f9b9626d6080e3ce5d7257093dd47
duridev <duridev@outlook.com>
2026-02-26 22:30:54 -0300

    Pass form data to exito template for display after successful submission

--- formulario/views.py
+++ formulario/views.py
@@ -7,7 +7,7 @@ def index(request):
         form = ContactoForm(request.POST)
         if form.is_valid():
             nombre = form.cleaned_data["nombre"]
-            return render(request, "formulario/exito.html")
+            return render(request, "formulario/exito.html", {'datos': form.cleaned_data })
     else:
         form = ContactoForm()
     return render(request, "formulario/index.html", {"form": form})
